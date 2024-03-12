8f822a96c5f7b1b644eccf43534d3a810d4c354c
fourbarman <maks.java@yandex.ru>
2024-03-12 10:32:51 +0400

    Entity lifecycle, 2 sessions. merge, refresh, delete, saveOrUpdate.

--- src/main/java/ru/fourbarman/HibernateRunner.java
+++ src/main/java/ru/fourbarman/HibernateRunner.java
@@ -4,64 +4,42 @@ package ru.fourbarman;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 import ru.fourbarman.converter.BirthdayConverter;
-import ru.fourbarman.entity.Birthday;
-import ru.fourbarman.entity.Role;
 import ru.fourbarman.entity.User;
-
-import java.time.LocalDate;
+import ru.fourbarman.util.HibernateUtil;
 
 public class HibernateRunner {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        //auto convert Birthday to Date
-        //configuration.addAttributeConverter(new BirthdayConverter(), true);
 
-        //OR without autoApply:true here - set @Converter(autoApply = true) on Converter Class and then:
-        configuration.addAttributeConverter(new BirthdayConverter());
+        User user = User.builder()
+                .username("ivan@gamil.com")
+                .lastname("Ivanov")
+                .firstname("Ivan")
+                .build();
 
-        //add JsonBinaryType type converter
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+             try (Session session1 = sessionFactory.openSession()) {
+                session1.beginTransaction();
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+                session1.saveOrUpdate(user);
 
-            User user = User.builder()
-                    .username("ivan11@gmail.com")
-                    .firstname("Ivan")
-                    .lastname("Ivanov")
-                    .info("""
-                            {
-                            "name": "Ivan",
-                            "id": 25
-                            }
-                            """)
-                    .role(Role.ADMIN)
-                    .birthDate(new Birthday(LocalDate.of(2000, 1, 1)))
-                    .build();
-            //session.save(user);
-//            session.update(user); will throw exception if not found!
-//            session.saveOrUpdate(user);
-//            session.delete(user);
-            User u = session.get(User.class, "ivan10@gmail.com");
+                session1.getTransaction().commit();
+            }
+            try (Session session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
 
-            //
-            u.setLastname("Renamed");
-            //session can be flushed to db
-            //session.flush();
+                user.setFirstname("Sveta");
+                //session2.delete(user);
 
-            //can detect session is dirty - entity was edited while session is open
-            //System.out.println(session.isDirty());
+                //refresh/merge
+                //session2.refresh(user); //refresh user from db
 
-            //how to clear session
-//            session.evict(u);
-//            session.clear();
-//            session.close();
+                Object mergedUser = session2.merge(user); //merge user to db
 
-            session.getTransaction().commit();
+                session2.getTransaction().commit();
+            }
         }
     }
 }
